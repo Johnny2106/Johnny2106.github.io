3db097c3ab77533f78b67e182b7f84dc98dcc2ca
John Beesoo <johnbeesoo@gmail.com>
2023-03-17 13:10:20 +0400

    update

--- assets/main.js
+++ assets/main.js
@@ -38,11 +38,3 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	    }
 	);
 	rss.render();
-
-    
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau", { responsive: true });
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
